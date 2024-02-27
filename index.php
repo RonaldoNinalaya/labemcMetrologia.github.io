28204285bd236d3b2315f419121b9a85faee0cae
RonaldoNinalaya <129538671+RonaldoNinalaya@users.noreply.github.com>
2024-02-26 23:47:54 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+
+  <?php include('./template/header.php') ?>
+  <link rel="stylesheet" href="/css/index.css">
+  
+  <div class="portada-principal">
+    
+  </div>
+
+  <main class="contenedor">
+      <h2>Hola mundo</h2>
+  </main>
+
+  <?php include('./template/footer.php')?>
